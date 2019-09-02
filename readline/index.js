a7e4af2fceb47e19970eb5981c6a59a18f8dde57
foreverZ <617754841@qq.com>
2019-09-02 21:24:53 +0800

    终于搞懂 readline 怎么用了

--- readline/index.js
+++ readline/index.js
@@ -1,37 +1,87 @@
 const readline = require('readline');
 const inquirer = require('inquirer');
+const sleep = (delay) => new Promise(resolve => setTimeout(resolve, delay));
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// prompt 修改行首文本
+// online 监听回车才算一行
+// (() => {
+//   const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+//     prompt: '请输入> '
+//   });
 
-const sleep = (delay) => {
-  return new Promise(resolve => {
-    setTimeout(resolve, delay);
+//   rl.prompt();
+//   setTimeout(() => {
+//     // write 时跟手动输入差不多，和 console 是不同的
+//     rl.write('hello\n');
+//   }, 500);
+
+//   rl.on('line', (line) => {
+//     line = line.trim();
+//     if (line.trim() === 'close') return rl.close();
+//     switch (line.trim()) {
+//       case 'hello':
+//         console.log('world!');
+//         break;
+//       default:
+//         console.log(`你输入的是：'${line.trim()}'`);
+//         break;
+//     }
+//     rl.prompt();
+//   }).on('close', () => {
+//     console.log('再见!');
+//     process.exit(0);
+//   });
+// })();
+
+(async () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
   });
-}
 
-rl.question('你如何看待 Node.js 中文网？', async (answer) => {
+  const questionAsync = (txt) => {
+    return new Promise(resolve => {
+      rl.question(txt, resolve);
+    });
+  }
 
-  console.log(`感谢您的宝贵意见：${answer}`);
+  const answer = await questionAsync('确定开始跑进度吗？(Y/N)');
+  const isYes = answer === '' || /y(es)?/i.test(answer)
+  if (!isYes) {
+    rl.write('\033[2J');
+    return process.exit(0);
+  }
 
-  rl.write('1s 后删除这行');
+  rl.write('开始删除\n');
 
-  await sleep(1000);
+  // 跑进度
+  (async function loop(index, max) {
+    if (index > max) return finish();
+    await sleep(1e2);
+    readline.cursorTo(process.stdout, 0);
+    readline.clearLine(process.stdout, 0);
+    rl.write('loading... ' + Math.ceil(index/max*100) + '%');
+    loop(++index, max);
+  })(0, 10);
 
-  // 模拟 Ctrl+u 删除先前写入的行。
-  rl.write(null, { ctrl: true, name: 'u' });
-  
+  // 跑完
+  function finish() {
+    readline.cursorTo(process.stdout, 0, 0);
+    rl.write('\033[2J');
+    rl.write('loading finish');
+    process.exit(0);
+  }
+
   // 清空输入后的部分
   // rl.write('\033[0f');
 
   // 全部清空
   // rl.write('\033[2J');
 
-  // rl.moveCursor(process.stdin, 0, 0);
-  // rl.clearLine(process.stdin, 0);
-  // rl.clearLine(process.stdout, 0);
+  // 模拟 Ctrl+u 删除先前写入的行。
+  // rl.write(null, { ctrl: true, name: 'u' });
 
-  rl.close();
-});
+  // rl.close();
+})();
