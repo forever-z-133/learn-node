554b0b96b5179011b87dafd6b0e0067897d9b803
foreverZ <617754841@qq.com>
2019-09-06 20:27:44 +0800

    重构了异步遍历方法

--- m3u8ToMp4/index.js
+++ m3u8ToMp4/index.js
@@ -10,51 +10,61 @@ const args = process.argv.slice(2);
  * 根据 m3u8 文件下载 ts，并拼成 mp4 文件
  */
 
-const [ url ] = args;
+const [url] = args;
 m3u8ToMp4(url, __dirname);
 // m3u8ToMp4('https://hd1.o0omvo0o.com/rh/898C29A1/SD/playlist.m3u8', __dirname);
 
 async function m3u8ToMp4(filePath, outputPath) {
+  const outputName = 'new';
+
   // 生成临时文件夹，存放临时下载的东西
   let tempDir = path.join(outputPath, "temp");
   !fs.existsSync(tempDir) && fs.mkdirSync(tempDir);
 
-  // 下载 m3u8 文件
-  const { txt } = await download(filePath, tempDir);
+  // 获取 m3u8 文件的内容
+  const m3u8FilePath = await download(filePath, tempDir);
+  const m3u8Content = fs.readFileSync(m3u8FilePath, 'utf8');
+
+  // 获取下载时的网上文件路径
   const postUrl = path.join(filePath, '../');
 
-  // 已完成的文件，比如中断等原因
-  const haslist = fs.readdirSync(tempDir);
-
   // 要下载的 ts 文件列表
-  let list = txt.match(/^.+\.ts/gm);
+  let list = m3u8Content.match(/^.+\.ts/gm);
   if (!list) throw new Error('匹配错误');
   list = list.map(item => new URL(item, postUrl).href);
 
+  // 已完成的文件，中断的话并不会清掉原文件，有点难搞
+  const haslist = fs.readdirSync(tempDir);
+
   // 下载小 ts 文件，且同时下载多条
-  forEachAsync(list, {
-    customAjax: async (url, next) => {
-      const fileName = (url.match(/\w+.ts/) || [])[0];
-      if (!fileName) throw new Error('名称有误');
+  forEachAsync(list, async (index, url, next) => {
+    const fileName = getFileName(url);
+    if (!fileName) throw new Error('名称有误');
 
-      console.log('download...', url);
-      if (haslist.includes(fileName)) return next(fileName);
+    if (haslist.includes(fileName)) return next(fileName);
 
-      await download(url, tempDir, fileName, () => {});
-      return next(fileName);
-    },
+    console.log('download...', url, `${Math.ceil(index/list.length*100)}%`);
+    await download(url, tempDir, fileName);
+    return next(fileName);
+  }, {
+    number: 8,
     finish: loaded
   });
 
   // 全部下载完成
   function loaded(fileNames) {
     console.log('files download finish');
-    _merge(fileNames, toMp4);
+    _merge(fileNames, () => {
+      const oname = outputName + '.ts';
+      fs.renameSync(path.join(tempDir, oname), path.join(outputPath, oname));
+      removeDirSync(tempDir);
+      tsToMp4();
+    });
   }
 
   // 合并小 ts 文件为 new.ts
   function _merge(fileNames, callback) {
-    let command = `copy /b ${fileNames.join('+')} new.ts`;
+    let command = `copy /b ${fileNames.join('+')} ${outputName}.ts`;
     const bat = child_process.spawn('cmd.exe', ['/c', command], {
       cwd: tempDir
     });
@@ -66,9 +76,9 @@ async function m3u8ToMp4(filePath, outputPath) {
   }
 
   // 将 new.ts 转为 new.mp4
-  function toMp4(callback) {
+  function tsToMp4(callback) {
 
-    child_process.spawn('explorer.exe', [tempDir]);
+    child_process.spawn('explorer.exe', [outputPath]);
     child_process.spawn('cmd.exe', ['/c', 'msg * "ts 转 mp4 还没完成，已可用格式工厂将 new.ts 转为想要的格式"']);
 
     // const ffmpeg_path = join('./libs/ffmpeg');
@@ -95,8 +105,30 @@ async function m3u8ToMp4(filePath, outputPath) {
   }
 }
 
+// 删除文件夹，包括其下所有文件及文件夹
+function removeDirSync(path) {
+  let files = [];
+  if (fs.existsSync(path)) {
+    files = fs.readdirSync(path);
+    files.forEach(file => {
+      let curPath = path + "/" + file;
+      if (fs.statSync(curPath).isDirectory()) {
+        delDir(curPath); //递归删除文件夹
+      } else {
+        fs.unlinkSync(curPath); //删除文件
+      }
+    });
+    fs.rmdirSync(path);
+  }
+}
+
+// 获取文件名称
+function getFileName(filePath) {
+  return filePath.slice(filePath.lastIndexOf('/') + 1);
+}
+
 // 下载文件
-function download(url, output, fileName, callback) {
+function download(url, output, fileName) {
   return new Promise(resolve => {
     fileName = fileName || url.slice(url.lastIndexOf('/'));
     if (!fileName) throw new Error('没找到文件名');
@@ -108,46 +140,33 @@ function download(url, output, fileName, callback) {
       });
       res.on('end', () => {
         stream.end();
-        const txt = fs.readFileSync(filePath, 'utf8');
-        callback ? callback() : resolve({ filePath, txt });
+        resolve(filePath);
       });
     });
   });
 }
 
-// 异步循环，但需要自己写 customAjax 异步过程
-function forEachAsync(data, options) {
+// 异步循环
+function forEachAsync(data, func, options) {
   options = options || {};
-  var timesConfig = Math.min(options.number || 5, 8); // 最大线程数
+  const timesConfig = Math.min(options.number || 5, 8); // 最大线程数
+  const total = data.length - 1;
+  const result = [];
 
-  var rest = timesConfig; // 剩余线程数
-  var current = 0; // 当前已进行索引（未必已完成）
-  var result = []; // 结果数据
+  let restQueue = timesConfig; // 剩余队列数
+  let started = 0; // 已发起
+  let loaded = 0; // 已完成
   (function loop(index) {
     const item = data[index];
     if (!item) return;
-    rest--;
-    if (rest < 0) return;
-
-    _ajax(item, function (res) {
-      rest++;
-      result[index] = res || item;
-      if (current > data.length - 1 && rest === timesConfig) return finish(result);
-      // 注意：_ajax 必须异步，不然这个 loop 会先于下面的 loop
-      setTimeout(() => {
-        loop(++current);
-      }, 1);
-    })
-
-    if (rest > 0) loop(++current);
+    func(index, item, res => {
+      restQueue++;
+      result[index] = res;
+      if (++loaded > total) return finish(result);
+      loop(++started);
+    });
+    if (--restQueue > 0) loop(++started);
   })(0);
-
-  // 每次一个数据，走完则回调
-  function _ajax(item, next) {
-    if (options.customAjax) {
-      return options.customAjax(item, next, current);
-    }
-  }
 
   // 全部运行完成
   function finish(result) {
